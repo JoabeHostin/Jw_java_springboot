e477772758e373b026c529905d9ed86917ff3b8b
JoabeHostin <105891888+JoabeHostin@users.noreply.github.com>
2023-01-30 18:31:23 -0300

    0.02
    Alteração no package view.

--- src/main/java/com/jw/jw_mecanica/repository/ClienteRepository.java
+++ src/main/java/com/jw/jw_mecanica/repository/ClienteRepository.java
@@ -1,11 +1,11 @@
 package com.jw.jw_mecanica.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.jw.jw_mecanica.models.Cliente;
 
-@Repository
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
