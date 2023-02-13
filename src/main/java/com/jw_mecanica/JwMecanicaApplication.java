794103a4df32bca2658e3f7072e6d4779c9f9718
Joabe Hostin <105891888+JoabeHostin@users.noreply.github.com>
2023-02-12 21:39:45 -0300

    1.0
    Criação de um objeto DTO, e implementação ao código existente. Aplicação estável.

--- src/main/java/com/jw_mecanica/JwMecanicaApplication.java
+++ src/main/java/com/jw_mecanica/JwMecanicaApplication.java
@@ -2,10 +2,6 @@ package com.jw_mecanica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import antlr.collections.List;
 
 
 
@@ -15,5 +11,8 @@ public class JwMecanicaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwMecanicaApplication.class, args);
 	}
+
+
+		
 	
 }
--- src/main/java/com/jw_mecanica/repository/ClienteRepository.java
+++ src/main/java/com/jw_mecanica/repository/ClienteRepository.java
@@ -2,10 +2,6 @@ package com.jw_mecanica.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 import com.jw_mecanica.models.Cliente;
 
 
